35a46a82cb5914660ba942f5fbde529547325bc8
aravindkv27 <aravind27032002@gmail.com>
2023-09-04 18:49:57 +0530

    Arrays added

---
+++ python_questions/Array.py
@@ -0,0 +1,20 @@
+from array import *
+
+# 1 Create an array and traverse
+
+arrays1 = array('i',[2,5,6,7,8,9,10])
+
+def traverse_arr(arrays1):
+
+    for i in arrays1:
+        print(i, end=" ")
+
+
+# traverse_arr(arrays1)
+
+# 2. Access Individual Elements through indexes
+
+print(arrays1[0])
+
+
+
